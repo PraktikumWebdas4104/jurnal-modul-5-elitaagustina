c41decf92b149b5d89e2b04813ac8351c595ebd4
elitaagustina <43506640+elitaagustina@users.noreply.github.com>
2018-10-05 10:22:47 +0700

    Update prosesregris.php
    ELITA AGUSTINA SITANGGANG
    6701174071
    D3MI4104

--- prosesregris.php
+++ prosesregris.php
@@ -1,4 +1,11 @@
-<?php
+<?php 
+session_start();
+include "koneksi.php";
 
+		$nama = $_POST['nama'];
+		$nim = $_POST['nim'];
+		$email = $_POST['email'];
 
-?>
+		echo "data berhasil disimpan";
+
+ ?>
