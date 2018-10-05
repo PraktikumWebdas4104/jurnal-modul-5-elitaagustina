1f15d006e79f01fbcf00689cff24eaead943545c
elitaagustina <43506640+elitaagustina@users.noreply.github.com>
2018-10-05 10:22:02 +0700

    Update regristrasi.php
    ELITA AGUSTINA SITANGGANG
    6701174071
    D3MI4104

--- regristrasi.php
+++ regristrasi.php
@@ -1,11 +1,25 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Form Registrasi</title>
-</head>
-<body>
-
-
-
-</body>
-</html>
+<form method="POST" action="koneksi.php">
+	<table>
+		<tr>
+			<td>NAMA</td>
+			<td>:</td>
+			<td><input type="text" name="nama"></td>
+		</tr>
+		<tr>
+			<td>NIM</td>
+			<td>:</td>
+			<td><input type="number" name="nim"></td>
+		</tr>
+		<tr>
+			<td>EMAIL</td>
+			<td>:</td>
+			<td><input type="email" name="email"></td>
+		</tr>
+		
+		<tr>
+			<td></td>
+			<td></td>
+			<td><input type="submit" name="submit" value="REGIS"></td>
+		</tr>
+	</table>
+</form>
