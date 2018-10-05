374f0f0c49bd77f6c2c8d058bda2349514d2ff39
elitaagustina <43506640+elitaagustina@users.noreply.github.com>
2018-10-05 10:24:31 +0700

    Update komentar.php
    ELITA AGUSTINA SITANGGANG
    6701174071
    D3MI4104

--- komentar.php
+++ komentar.php
@@ -1,3 +1,26 @@
+<form method="POST">
+	<table>
+		<tr>
+			<td>KOMENTAR</td>
+			<td>:</td>
+			<td><input type="text" name="komen"></td>
+		</tr>
+		<tr>
+			<td></td>
+			<td></td>
+			<td><input type="submit" name="submitReg" value="MASUK"></td>
+		</tr>
+	</table>
+</form>
+
 <?php
+	if (isset($_POST['komen'])) {
+		$kata = $_POST['kata'];
+
+		if (strlen($komen)!=10) {
+			echo "komentar yang anda inputkan kurang ";
+		}
+	}
+
 
 ?>
