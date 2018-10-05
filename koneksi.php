e6840e2781a07769465cb55fc18d02687143d25e
elitaagustina <43506640+elitaagustina@users.noreply.github.com>
2018-10-05 10:23:38 +0700

    Update koneksi.php
    ELITA AGUSTINA SITANGGANG
    6701174071
    D3MI4104

--- koneksi.php
+++ koneksi.php
@@ -1,3 +1,33 @@
 <?php
+$koneksi = new mysqli("localhost", "root", "", "mahasiswa");
 
-?>
+
+  			if (isset($_POST['submit'])) {
+		
+
+				if ($koneksi) {
+					$nama = $_POST['nama'];
+					$nim = $_POST['nim'];
+					$email = $_POST['email'];
+					
+					$status=true;
+	
+					if (!is_int($nim) && strlen($nim)!=10 && strlen($nama)!=25) {
+						echo "inputan salah";
+						$status=false;
+					}
+
+					if ($status) {
+							$sql = $koneksi->query("INSERT INTO 'mahasiswa' ('nama', 'nim', 'email') VALUES ('".$nama."', '".$nim."', '".$email."')");
+					
+					echo "REGISTRASI SUKSES<br>";
+
+					}
+				}else{
+					echo "REGISTRASI GAGAL";
+				}
+			}
+
+
+  		
+ ?>
